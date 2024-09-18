3bf16707872fdd0ef244b79525ed2dc1804c67b8
Nat Richmond <natrichmond@Nats-MacBook-Pro.local>
2024-09-18 03:05:00 -0400

    grade calculator

---
+++ GradeCalculator.js
@@ -0,0 +1,20 @@
+let average;
+let i;
+let sum;
+
+function getAverage(scores) {
+  sum = 0;
+   average = 0;
+ for (i = 0; i < scores.length; i++) {
+   sum += scores[i];
+   average = sum / scores.length;
+  }
+
+ return average
+}
+
+
+
+console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+console.log(getAverage([38, 99, 87, 100, 100, 100, 100, 100, 100, 100]))
