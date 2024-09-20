2c80475a45e4094fd753858d93255e4c2c486f44
Nat Richmond <natrichmond@Nats-MacBook-Pro.local>
2024-09-19 22:45:25 -0400

    role playing game setup

---
+++ GradebookMessage.js
@@ -0,0 +1,50 @@
+function getAverage(scores) {
+    let sum = 0;
+  
+    for (const score of scores) {
+      sum += score;
+    }
+  
+    return sum / scores.length;
+  }
+  
+  function getGrade(score) {
+    if (score === 100) {
+      return "A++";
+    } else if (score >= 90) {
+      return "A";
+    } else if (score >= 80) {
+      return "B";
+    } else if (score >= 70) {
+      return "C";
+    } else if (score >= 60) {
+      return "D";
+    } else {
+      return "F";
+    }
+  }
+  
+  function hasPassingGrade(score) {
+    return getGrade(score) !== "F";
+  }
+  
+  let classAvg;
+  let stuAvg;
+  let didPass;
+  
+  function studentMsg(totalScores, studentScore) {
+    classAvg = getAverage(totalScores);
+    stuAvg = getGrade(studentScore);
+    didPass = hasPassingGrade(studentScore);
+    
+    if (didPass === false) {
+      return `Class average: ${classAvg}. Your grade: ${stuAvg}. You failed the course.`
+  } else {
+    return `Class average: ${classAvg}. Your grade: ${stuAvg}. You passed the course.`
+  
+  }
+  }
+  
+  
+  console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
+  
