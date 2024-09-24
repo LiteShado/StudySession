dec9b816c23f98017f578fb1373fdd2664ec49ab
Nat Richmond <natrichmond@Nats-MacBook-Pro.local>
2024-09-24 19:53:18 -0400

    sellWeapon function complete

--- RolePlayingGame/script.js
+++ RolePlayingGame/script.js
@@ -118,12 +118,26 @@ function buyHealth() {
 
 
 function buyWeapon() {
+    if (currentWeaponIndex < weapons.length - 1) {
     if (gold >= 30) {
       gold -= 30;
       currentWeaponIndex++;
+    
       goldText.innerText = gold;
-    text.innerText = "You now have a new weapon."
+
+      let newWeapon = weapons[currentWeaponIndex].name;
+    text.innerText = "You now have a " + newWeapon + ".";
+    inventory.push(newWeapon);
+    text.innerText += " In your inventory you have: " + inventory;
+    } else {
+        text.innerText = "You do not have enough gold to buy a weapon.";
     }
+} else {
+    text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold";
+    button2.onclick = sellWeapon;
+    
+}
 }
 
 function goTown() {
@@ -136,26 +150,29 @@ function goTown() {
     text.innerText = "You are in the town square. You see a sign that says \"Store\".";
   }
 
+function sellWeapon() {
+    if (inventory.length > 1) {
+      gold += 15;
+      goldText.innerText = gold;
+      let currentWeapon = inventory.shift();
+      text.innerText = "You sold a " + currentWeapon + ".";
+      text.innerText += " In your inventory you have: "+ inventory;
+    } else {
+        text.innerText = "Don't sell your only weapon!";
+    }
+}
+
 function goStore() {
         update(locations[1]);
 
   }
 
-function goCave() {
-
-}
 
 function fightDragon() {
 
   }
 
-  function buyHealth() {
-
-  }
-  
-  function buyWeapon() {
-  
-  }
+ 
   
 
   function fightSlime() {
